7e128ec3c359a9b8a850bb1f655b4780513c69de
León <argon5571@gmail.com>
2024-11-09 01:54:26 -0300

    Fixed Anaconda

--- SFR/build.bat
+++ SFR/build.bat
@@ -1,1 +1,8 @@
+SET SFD="C:\Data\Software\Steam\steamapps\common\Superfighters Deluxe"
+cd ..
 
+copy %1\SFR.exe.config %SFD%
+copy %1\SFR.exe %SFD%
+copy %1\0Harmony.dll %SFD%\SFR
+copy %1\DiscordRPC.dll %SFD%\SFR
+copy %1\Newtonsoft.Json.dll %SFD%\SFR
